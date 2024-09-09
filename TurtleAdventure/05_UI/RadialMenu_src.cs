fbacf13a2c25f536416fc2e71a021db5aee0189d
kosuke-Hanchan <kosuke.reon@gmail.com>
2024-09-10 00:36:25 +0900

    円形メニューのボタンにアイコン画像を表示するよう変更

--- TurtleAdventure/05_UI/RadialMenu_src.cs
+++ TurtleAdventure/05_UI/RadialMenu_src.cs
@@ -6,8 +6,9 @@ public class RadialMenu_src : MonoBehaviour
 /*------------- インスペクター設定用変数 --------------*/
     [SerializeField] GameObject go_g_buttonPrefab;      // ボタンのプレハブ
     [SerializeField] Button bt_g_toggleButton;          // メニュー開閉用のボタン
-    [SerializeField] float fl_l_buttonRadius = 100f;    // ボタンの半径
-
+    [SerializeField] float fl_l_buttonRadius = 100f;    // ボタン生成位置の半径
+    [SerializeField] Sprite[] buttonImages;             // 各ボタンに設定する画像
+    [SerializeField] Vector2 buttonSize = new Vector2(150f, 150f); // 生成するボタンのサイズ
 
 /*--------------- 定数 ----------------*/
     private const uint u1_g_numberOfButtons = 5;   // ボタンの数
@@ -54,9 +55,23 @@ public class RadialMenu_src : MonoBehaviour
             GameObject button = Instantiate(go_g_buttonPrefab, transform);
             button.transform.localPosition = new Vector3(xPos, yPos, 0);
 
-            // ボタンに名前を付ける（任意）
+            // ボタンのサイズを設定
+            RectTransform rectTransform = button.GetComponent<RectTransform>();
+            if (rectTransform != null)
+            {
+                rectTransform.sizeDelta = buttonSize;
+            }
+            
+            // ボタンに名前を付ける
             button.name = "Button " + (i + 1);
-
+            
+            // ボタンに画像を設定
+            Image buttonImage = button.GetComponent<Image>();
+            if (buttonImages.Length > i)
+            {
+                buttonImage.sprite = buttonImages[i];
+            }
+            
             // ボタンにクリックイベントを追加
             int index = i; // クロージャ問題を回避するためにローカル変数を使用
             button.GetComponent<Button>().onClick.AddListener(() => OnButtonClick(index));
@@ -64,7 +79,9 @@ public class RadialMenu_src : MonoBehaviour
     }
 
 
-
+    /// <summary>
+    /// 
+    /// </summary>
     void ToggleMenu()
     {
         // IF:プレイヤー状態が"待機状態"か
@@ -85,6 +102,11 @@ public class RadialMenu_src : MonoBehaviour
         Time.timeScale = isMenuOpen ? 0 : 1;
     }
 
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="isActive"></param>
     void SetMenuActive(bool isActive)
     {
         // 子要素（ボタン）の表示/非表示を設定
@@ -94,6 +116,11 @@ public class RadialMenu_src : MonoBehaviour
         }
     }
 
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="index"></param>
     void OnButtonClick(int index)
     {
         // ボタンがクリックされたときの処理を設定
@@ -103,7 +130,7 @@ public class RadialMenu_src : MonoBehaviour
         isMenuOpen = false;
         Time.timeScale = 1;
         
-        // ここにボタンごとの処理を追加
+        // ボタンごとの処理を追加
         switch (index)
         {
             // ボタン1の処理
